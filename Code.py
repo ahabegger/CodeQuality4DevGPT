f568d0b4e16ef5001c97bb77c2fcd4257e4abee2
habeggaj <habeggaj@miamioh.edu>
2024-02-22 15:11:36 -0500

    Added Linting function to Code Object

--- Code.py
+++ Code.py
@@ -3,7 +3,12 @@ Code.py
 This file defines a Code class that will be used to store the code objects from the DevGPT Dataset
 """
 
-# Defining the Code class
+# Importing the external dependencies
+import subprocess
+import tempfile
+import re
+
+
 class Code:
     def __init__(self, code_content, code_language, model_used, title):
         self.code_content = code_content
@@ -22,4 +27,30 @@ class Code:
     def is_language(self, language):
         return self.code_language == language
 
+    def get_code_extension(self):
+        if self.code_language == "python":
+            return "py"
+        elif self.code_language == "java":
+            return "java"
+        elif self.code_language == "javascript":
+            return "js"
+        else:
+            return "txt"
 
+    def lint_code(self):
+        # Create a temporary file
+        with tempfile.NamedTemporaryFile(suffix='.' + self.code_language, delete=False) as temp:
+            # Write the code_content to the temporary file
+            temp.write(self.code_content.encode())
+            temp.flush()
+
+            # Run coala on the temporary file
+            result = subprocess.run(['coala', '--files', temp.name, '--json'], capture_output=True, text=True)
+
+            # Parse the output of coala to get the number of errors and error types
+            output = result.stdout
+            error_pattern = re.compile(r'"origin": "(.*?)", "message": "(.*?)"')
+            errors = error_pattern.findall(output)
+
+            # Return the number of errors and error types
+            return len(errors), {origin: sum(1 for error in errors if error[0] == origin) for origin, _ in errors}
