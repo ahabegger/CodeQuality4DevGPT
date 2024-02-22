6b47d7c27ecdd02e7866aa174de66e575393f767
habeggaj <habeggaj@miamioh.edu>
2024-02-22 14:47:55 -0500

    Extracting data from all sharing JSON files

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """
 main.py
-- This is the main file that will be run to start the program
+This is the main file that will be run to start the program
 """
 
-# Importing the required modules
+# Importing the external dependencies
 import json
+
+# Importing the internal dependencies
+from Extract import extract_data
 
 # Getting data from the DevGPT Dataset
 commit_sharings = json.load(open('DevGPT_Dataset/20231012_230826_commit_sharings.json', 'r'))
@@ -14,15 +17,20 @@ file_sharings = json.load(open('DevGPT_Dataset/20231012_234250_file_sharings.jso
 issue_sharings = json.load(open('DevGPT_Dataset/20231012_235128_issue_sharings.json', 'r'))
 discussion_sharings = json.load(open('DevGPT_Dataset/20231012_235320_discussion_sharings.json', 'r'))
 
-# Define Empty List to store the code objects
-code_objects = []
+# Define List to store the code objects
+code_objects = extract_data(commit_sharings)
+code_objects += extract_data(hn_sharings)
+code_objects += extract_data(pr_sharings)
+code_objects += extract_data(file_sharings)
+code_objects += extract_data(issue_sharings)
+code_objects += extract_data(discussion_sharings)
 
-
+# Find the type of code objects
 python_code_objects = [code for code in code_objects if code.is_language("python")]
 java_code_objects = [code for code in code_objects if code.is_language("java")]
 javascript_code_objects = [code for code in code_objects if code.is_language("javascript")]
 
-
+# Print the number of code objects
 print(f"Python Code Objects: {len(python_code_objects)}")
 print(f"Java Code Objects: {len(java_code_objects)}")
 print(f"Javascript Code Objects: {len(javascript_code_objects)}")
